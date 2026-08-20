5e4bf6bb582fae6fc0ec9bb5cd3ddd91e2b5fac7
suryavirkumar545-dev <suryavirkumar545@gmail.com>
2026-08-20 21:51:59 +0530

    Randomize photo selection in start.py

--- plugins/start.py
+++ plugins/start.py
@@ -220,7 +220,7 @@ async def start_command(client: Client, message: Message):
             ]
         )
         await message.reply_photo(
-            photo=START_PIC,
+            photo=random.choice(START_PICS),
             caption=START_MSG.format(
                 first=message.from_user.first_name,
                 last=message.from_user.last_name,
