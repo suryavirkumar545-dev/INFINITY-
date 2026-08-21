73a08610cd2c521a84aebba5840b48413a9a0036
suryavirkumar545-dev <suryavirkumar545@gmail.com>
2026-08-21 07:01:46 +0530

    Refactor config.py to improve readability and structure

--- config.py
+++ config.py
@@ -1,139 +1,71 @@
-# Don't Remove Credit @CodeFlix_Bots, @rohit_1888
-# Ask Doubt on telegram @CodeflixSupport
-#
-# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
-#
-# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
-# and is released under the MIT License.
-# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 import os
-from os import environ,getenv
+from os import environ, getenv
 import logging
 from logging.handlers import RotatingFileHandler
 
-#rohit_1888 on Tg
-#--------------------------------------------
-#Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8751517852:AAFTjRgxwLq-ETAZGzSukaBGpPkm83R6Gps")
-APP_ID = int(os.environ.get("APP_ID", "29780235"))
-API_HASH = os.environ.get("API_HASH", "5d3404de54587fb6eec7f1aef656018d")
-#--------------------------------------------
+# ---- Bot credentials (GitHub push se pehle env vars me daalo) ----
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+APP_ID       = int(os.environ.get("APP_ID", "0"))
+API_HASH     = os.environ.get("API_HASH", "")
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003734749769"))
-OWNER = os.environ.get("OWNER", "SKANIME04")  # Owner username without @
-OWNER_ID = int(os.environ.get("OWNER_ID", "7032769404")) # Owner id
-#--------------------------------------------
-PORT = os.environ.get("PORT", "8001")
-BASE_URL = os.environ.get("BASE_URL", "https://infinity-op8p.onrender.com/")  # e.g. https://your-domain.com
-#--------------------------------------------
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://FilestoreDB:Darkauli%401234@infinityfile.pitecuw.mongodb.net/?appName=Infinityfile")
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003734749769"))   # DB channel
+OWNER      = os.environ.get("OWNER", "SKANIME04")                 # owner username (no @)
+OWNER_ID   = int(os.environ.get("OWNER_ID", "7032769404"))
+
+PORT     = os.environ.get("PORT", "8001")
+BASE_URL = os.environ.get("BASE_URL", "https://infinity-op8p.onrender.com/")
+
+DB_URI  = os.environ.get("DATABASE_URL", "")
 DB_NAME = os.environ.get("DATABASE_NAME", "FilestoreDB")
-#--------------------------------------------
-FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "10"))  # 0 means no expiry
-BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/SKANIME04")
-TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "200"))
-#--------------------------------------------
+
+FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "10"))
+BAN_SUPPORT      = os.environ.get("BAN_SUPPORT", "https://t.me/SKANIME04")   # contact support
+TG_BOT_WORKERS   = int(os.environ.get("TG_BOT_WORKERS", "200"))
+
 # SK ANIME FILE STORE — 5 random start banners (16:9)
-START_PICS = os.environ.get("START_PICS", "https://project--c5b9624b-32d2-48f1-82aa-208313351e70-dev.lovable.app/thumbs/sk-1.jpg https://project--c5b9624b-32d2-48f1-82aa-208313351e70-dev.lovable.app/thumbs/sk-2.jpg https://project--c5b9624b-32d2-48f1-82aa-208313351e70-dev.lovable.app/thumbs/sk-3.jpg https://project--c5b9624b-32d2-48f1-82aa-208313351e70-dev.lovable.app/thumbs/sk-4.jpg https://project--c5b9624b-32d2-48f1-82aa-208313351e70-dev.lovable.app/thumbs/sk-5.jpg").split()
+BANNER_BASE = "https://project--c5b9624b-32d2-48f1-82aa-208313351e70-dev.lovable.app/thumbs"
+START_PICS = os.environ.get("START_PICS", " ".join(f"{BANNER_BASE}/sk-{i}.jpg" for i in range(1, 6))).split()
 START_PIC = START_PICS[0]
 FORCE_PIC = os.environ.get("FORCE_PIC", START_PICS[1])
 
-# Force-Subscribe main channel (username or -100 id). Always required.
+# Force-Subscribe main channel
 FSUB_CHANNEL = os.environ.get("FSUB_CHANNEL", "@CARTOONFUNNY04")
 
-#--------------------------------------------
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "https://shortxlinks.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "0fbaf68e49013099204197ba4ed03a01c7196386")
-TUT_VID = os.environ.get("TUT_VID","https://t.me/H0W_2_USE/12")
-SHORT_MSG = "<b>⌯ Here is Your Download Link, Must Watch Tutorial Before Clicking On Download...</b>"
-
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "")
+TUT_VID       = os.environ.get("TUT_VID", "https://t.me/CARTOONFUNNY04")
+SHORT_MSG     = "<b>⌯ Here is Your Download Link, Must Watch Tutorial Before Clicking On Download...</b>"
 SHORTENER_PIC = os.environ.get("SHORTENER_PIC", "https://telegra.ph/file/ec17880d61180d3312d6a.jpg")
-#--------------------------------------------
 
-#--------------------------------------------
-HELP_TXT = "<b><blockquote>ᴛʜɪs ɪs ᴀɴ ғɪʟᴇ ᴛᴏ ʟɪɴᴋ ʙᴏᴛ ᴡᴏʀᴋ ғᴏʀ @CARTOONFUNNY04\n\n❏ ʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅs\n├/start : sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ\n├/about : ᴏᴜʀ Iɴғᴏʀᴍᴀᴛɪᴏɴ\n└/help : ʜᴇʟᴘ ʀᴇʟᴀᴛᴇᴅ ʙᴏᴛ\n\n sɪᴍᴘʟʏ ᴄʟɪᴄᴋ ᴏɴ ʟɪɴᴋ ᴀɴᴅ sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ ᴊᴏɪɴ ʙᴏᴛʜ ᴄʜᴀɴɴᴇʟs ᴀɴᴅ ᴛʀʏ ᴀɢᴀɪɴ ᴛʜᴀᴛs ɪᴛ.....!\n\n ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ <a href=https://t.me/CARTOONFUNNY04>ᴍᴏᴅᴀᴘᴘsᴋɪɴɢ</a></blockquote></b>"
-ABOUT_TXT = "<b><blockquote>◈ ᴄʀᴇᴀᴛᴏʀ: <a href=https://t.me/CARTOONFUNNY04>CARTOONFUNNY04</a>\n◈ ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/+i6NIEXiiQWM2YTll>ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ</a>\n◈ ᴇᴅᴜ ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/EduAppsKing>EduAppsKing</a>\n◈ ᴄᴏᴜʀsᴇ ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/FATHER_OF_COURSE>Father Of Course</a>\n◈ ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ : <a href=https://t.me/SKANIME04>SKANIME04</a></blockquote></b>"
-#--------------------------------------------
-#--------------------------------------------
-START_MSG = os.environ.get("START_MESSAGE", "<b>ʜᴇʟʟᴏ {mention}\n\n<blockquote> ɪ ᴀᴍ ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ, ɪ ᴄᴀɴ sᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇs ɪɴ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜsᴇʀs ᴄᴀɴ ᴀᴄᴄᴇss ɪᴛ ғʀᴏᴍ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ.</blockquote></b>")
+HELP_TXT  = "<b><blockquote>ᴛʜɪs ɪs ᴀɴ ғɪʟᴇ ᴛᴏ ʟɪɴᴋ ʙᴏᴛ ᴡᴏʀᴋ ғᴏʀ @CARTOONFUNNY04\n\n❏ ʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅs\n├/start : sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ\n├/about : ᴏᴜʀ Iɴғᴏʀᴍᴀᴛɪᴏɴ\n└/help : ʜᴇʟᴘ ʀᴇʟᴀᴛᴇᴅ ʙᴏᴛ\n\n ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ <a href=https://t.me/SKANIME04>@SKANIME04</a></blockquote></b>"
+ABOUT_TXT = "<b><blockquote>◈ ʙᴏᴛ : SK ANIME FILE STORE\n◈ ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/CARTOONFUNNY04>@CARTOONFUNNY04</a>\n◈ ᴜᴘᴅᴀᴛᴇs : <a href=https://t.me/CARTOONFUNNY04>@CARTOONFUNNY04</a>\n◈ sᴜᴘᴘᴏʀᴛ : <a href=https://t.me/SKANIME04>@SKANIME04</a>\n◈ ᴅᴇᴠᴇʟᴏᴘᴇʀ / ᴏᴡɴᴇʀ : <a href=https://t.me/SKANIME04>@SKANIME04</a></blockquote></b>"
+
+START_MSG = os.environ.get("START_MESSAGE", "<b>ʜᴇʟʟᴏ {mention}\n\n<blockquote> ɪ ᴀᴍ <b>SK ANIME FILE STORE</b> ʙᴏᴛ, ɪ ᴄᴀɴ sᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇs ɪɴ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜsᴇʀs ᴄᴀɴ ᴀᴄᴄᴇss ɪᴛ ғʀᴏᴍ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ.</blockquote></b>")
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "ʜᴇʟʟᴏ {mention}\n\n<b><blockquote>ᴊᴏɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟs ᴀɴᴅ ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ʀᴇʟᴏᴀᴅ button ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ʀᴇǫᴜᴇꜱᴛᴇᴅ ꜰɪʟᴇ.</b></blockquote>")
 
-CMD_TXT = """<blockquote><b>» ᴀᴅᴍɪɴ ᴄᴏᴍᴍᴀɴᴅs:</b></blockquote>
-
-<b>›› /dlt_time :</b> sᴇᴛ ᴀᴜᴛᴏ ᴅᴇʟᴇᴛᴇ ᴛɪᴍᴇ
-<b>›› /check_dlt_time :</b> ᴄʜᴇᴄᴋ ᴄᴜʀʀᴇɴᴛ ᴅᴇʟᴇᴛᴇ ᴛɪᴍᴇ
-<b>›› /dbroadcast :</b> ʙʀᴏᴀᴅᴄᴀsᴛ ᴅᴏᴄᴜᴍᴇɴᴛ / ᴠɪᴅᴇᴏ
-<b>›› /ban :</b> ʙᴀɴ ᴀ ᴜꜱᴇʀ
-<b>›› /unban :</b> ᴜɴʙᴀɴ ᴀ ᴜꜱᴇʀ
-<b>›› /banlist :</b> ɢᴇᴛ ʟɪsᴛ ᴏꜰ ʙᴀɴɴᴇᴅ ᴜꜱᴇʀs
-<b>›› /addchnl :</b> ᴀᴅᴅ ꜰᴏʀᴄᴇ sᴜʙ ᴄʜᴀɴɴᴇʟ
-<b>›› /delchnl :</b> ʀᴇᴍᴏᴠᴇ ꜰᴏʀᴄᴇ sᴜʙ ᴄʜᴀɴɴᴇʟ
-<b>›› /listchnl :</b> ᴠɪᴇᴡ ᴀᴅᴅᴇᴅ ᴄʜᴀɴɴᴇʟs
-<b>›› /fsub_mode :</b> ᴛᴏɢɢʟᴇ ꜰᴏʀᴄᴇ sᴜʙ ᴍᴏᴅᴇ
-<b>›› /pbroadcast :</b> sᴇɴᴅ ᴘʜᴏᴛᴏ ᴛᴏ ᴀʟʟ ᴜꜱᴇʀs
-<b>›› /add_admin :</b> ᴀᴅᴅ ᴀɴ ᴀᴅᴍɪɴ
-<b>›› /deladmin :</b> ʀᴇᴍᴏᴠᴇ ᴀɴ ᴀᴅᴍɪɴ
-<b>›› /admins :</b> ɢᴇᴛ ʟɪsᴛ ᴏꜰ ᴀᴅᴍɪɴs
-<b>›› /addpremium :</b> ᴀᴅᴅ ᴀ ᴘʀᴇᴍɪᴜᴍ ᴜꜱᴇʀ
-<b>›› /premium_users :</b> ʟɪsᴛ ᴀʟʟ ᴘʀᴇᴍɪᴜᴍ ᴜꜱᴇʀs
-<b>›› /remove_premium :</b> ʀᴇᴍᴏᴠᴇ ᴘʀᴇᴍɪᴜᴍ ꜰʀᴏᴍ ᴀ ᴜꜱᴇʀ
-<b>›› /myplan :</b> ᴄʜᴇᴄᴋ ʏᴏᴜʀ ᴘʀᴇᴍɪᴜᴍ sᴛᴀᴛᴜs
-<b>›› /count :</b> ᴄᴏᴜɴᴛ ᴠᴇʀɪғɪᴄᴀᴛɪᴏɴs
-<b>›› /delreq :</b> Rᴇᴍᴏᴠᴇᴅ ʟᴇғᴛᴏᴠᴇʀ ɴᴏɴ-ʀᴇǫᴜᴇsᴛ ᴜsᴇʀs
-"""
-#--------------------------------------------
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>• ʙʏ @CARTOONFUNNY04</b>") #set your Custom Caption here, Keep None for Disable Custom Caption
-PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False #set True if you want to prevent users from forwarding files from bot
-#--------------------------------------------
-#Set true if you want Disable your Channel Posts Share button
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>• ʙʏ @CARTOONFUNNY04</b>")
+PROTECT_CONTENT = os.environ.get('PROTECT_CONTENT', "False") == "True"
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
-#--------------------------------------------
-BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
+
+BOT_STATS_TEXT  = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "😂 Bhai seedha baat mat kar, /start dabao!"
 
-#==========================(BUY PREMIUM)====================#
-
-OWNER_TAG = os.environ.get("OWNER_TAG", "SKANIME04")
-UPI_ID = os.environ.get("UPI_ID", "20213904@axl")
-QR_PIC = os.environ.get("QR_PIC", "https://i.ibb.co/35QF8QMJ/QR.jpg")
+OWNER_TAG      = os.environ.get("OWNER_TAG", "SKANIME04")
+UPI_ID         = os.environ.get("UPI_ID", "20213904@axl")
+QR_PIC         = os.environ.get("QR_PIC", "https://i.ibb.co/35QF8QMJ/QR.jpg")
 SCREENSHOT_URL = os.environ.get("SCREENSHOT_URL", "t.me/SKANIME04")
-#--------------------------------------------
-#Time and its price
-#7 Days
-PRICE1 = os.environ.get("PRICE1", "0 rs")
-#1 Month
-PRICE2 = os.environ.get("PRICE2", "60 rs")
-#3 Month
-PRICE3 = os.environ.get("PRICE3", "150 rs")
-#6 Month
-PRICE4 = os.environ.get("PRICE4", "280 rs")
-#1 Year
-PRICE5 = os.environ.get("PRICE5", "550 rs")
 
-#===================(END)========================#
+PRICE1, PRICE2, PRICE3 = os.environ.get("PRICE1", "0 rs"), os.environ.get("PRICE2", "60 rs"), os.environ.get("PRICE3", "150 rs")
+PRICE4, PRICE5 = os.environ.get("PRICE4", "280 rs"), os.environ.get("PRICE5", "550 rs")
 
 LOG_FILE_NAME = "filesharingbot.txt"
-
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
     datefmt='%d-%b-%y %H:%M:%S',
-    handlers=[
-        RotatingFileHandler(
-            LOG_FILE_NAME,
-            maxBytes=50000000,
-            backupCount=10
-        ),
-        logging.StreamHandler()
-    ]
+    handlers=[RotatingFileHandler(LOG_FILE_NAME, maxBytes=50000000, backupCount=10), logging.StreamHandler()]
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
-   
